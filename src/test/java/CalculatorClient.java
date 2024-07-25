145f0963625fe88a81720a56b37d990c0d551d29
Johnny Jiang <giovannilance94@gmail.com>
2024-07-25 16:00:27 +0930

    chore: 🚚 rename client file

--- src/test/java/CalculatorClient.java
+++ src/test/java/CalculatorClient.java
@@ -9,7 +9,7 @@ import java.util.concurrent.CountDownLatch;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
-public class CalculatorClientTest {
+public class CalculatorClient {
     private Calculator calculator;
 
     private static final int CLIENT_COUNT = 4;
